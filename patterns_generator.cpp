cf233413eefd1ad1b80ab4f3bede5926b0ea9682
amelian15 <amelianiedzwiadek15@gmail.com>
2026-01-29 20:29:50 +0100

    komentarze
    Updated the DNA pattern generator to include detailed comments and improve readability. Added documentation for functions and clarified the purpose of the program.

--- patterns_generator.cpp
+++ patterns_generator.cpp
@@ -1,18 +1,21 @@
-// Generator wzorców DNA z dziurami zgodnych z systemem:
-//   • '.' – pojedyncza dziura
-//   • losowe 'N'
-//   • pobieranie fragmentów z prawdziwej sekwencji FASTA
-//
-// Służy do tworzenia dużych zestawów testowych.
-// Automatycznie generuje 3 zestawy:
-//   10 wzorców długości 10
-//   50 wzorców długości 12
-//   200 wzorców długości 20
+/**
+ * @file pattern_generator.cpp
+ * @author Maria Ławniczak (Nr Indeksu: 268544)
+ * @brief Generator zestawów testowych wzorców DNA z uwzględnieniem luk (wildcards)
+ * Narzędzie ekstrahuje fragmenty z plików FASTA i wprowadza do nich znaki '.' (dowolny nukleotyd)
+ * @date 2026-01-25
+ */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-// Wczytywanie FASTA do pojedynczego stringa DNA
+/**
+ * @brief Parsuje plik w formacie FASTA do pojedynczego ciągu znaków
+ * Funkcja ignoruje nagłówki (linie zaczynające się od '>') oraz wszelkie białe znaki,
+ * normalizując sekwencję do wielkich liter.
+ * * @param path Ścieżka do pliku wejściowego .fasta lub .fa
+ * @return string Połączona sekwencja nukleotydowa
+ */
 string load_fasta(const string &path){
     ifstream in(path);
     if(!in){
@@ -22,11 +25,13 @@ string load_fasta(const string &path){
     string line, acc, result;
     while(getline(in, line)){
         if(!line.empty() && line[0] == '>'){
+            // Obsługa wielu rekordów w jednym pliku FASTA
             if(!acc.empty()){
                 result += acc;
                 acc.clear();
             }
         } else {
+            // Czyszczenie linii z białych znaków i dodawanie do akumulatora
             for(char c: line)
                 if(!isspace((unsigned char)c))
                     acc.push_back(toupper(c));
@@ -36,14 +41,23 @@ string load_fasta(const string &path){
     return result;
 }
 
-// Dodawanie dziur '.' do wzorca z szansą ~gap_frac
+/**
+ * @brief Losowo wprowadza znaki maskowania (dziury) do wzorca
+ * Zamienia wybraną liczbę nukleotydów na kropki ('.') zgodnie z zadaną frakcją
+ * * @param s Oryginalna sekwencja DNA
+ * @param gap_frac Prawdopodobieństwo/udział luk w sekwencji (0.0 - 1.0)
+ * @param rng Referencja do generatora liczb losowych
+ * @return string Zmodyfikowany wzorzec z dziurami
+ */
 string add_gaps(string s, double gap_frac, mt19937 &rng){
     if(gap_frac <= 0)
         return s;
 
+    // Obliczanie docelowej liczby luk (minimum 1, jeśli frakcja > 0)
     int to_gap = max(1, (int)round(s.size()*gap_frac));
     uniform_int_distribution<int> dist(0, s.size()-1);
 
+    // Losowanie unikalnych pozycji dla dziur
     unordered_set<int> used;
     while((int)used.size() < to_gap)
         used.insert(dist(rng));
@@ -54,14 +68,23 @@ string add_gaps(string s, double gap_frac, mt19937 &rng){
     return s;
 }
 
-// Zapis listy wzorców do pliku
+/**
+ * @brief Zapisuje wygenerowane wzorce do pliku tekstowego (jeden wzorzec w linii)
+ * * @param pats Wektor wygenerowanych wzorców
+ * @param path Ścieżka docelowa zapisu
+ */
 void save_patterns(const vector<string> &pats, const string &path){
     ofstream out(path);
     for(const string &p: pats)
         out << p << "\n";
 }
 
-// MAIN
+/**
+ * @brief Punkt wejścia programu - zarządza generowaniem zestawów testowych
+ * Obsługuje 3 predefiniowane konfiguracje wielkości i długości wzorców
+ * * @param argc Liczba argumentów (wymagane: fasta, prefix)
+ * @param argv Tablica argumentów (fasta, prefix, opcjonalnie gap_fraction)
+ */
 int main(int argc, char **argv){
     if(argc < 3){
         cerr << "Usage: " << argv[0] << " <fasta> <prefix> [gap_fraction]\n";
@@ -70,18 +93,21 @@ int main(int argc, char **argv){
 
     string fasta = argv[1];
     string prefix = argv[2];
+    // Domyślna frakcja dziur wynosi 0.2 (20%), jeśli nie podano inaczej
     double gap_frac = (argc >= 4) ? stod(argv[3]) : 0.2;
 
     string text = load_fasta(fasta);
-    mt19937 rng(123456);  // deterministyczne generowanie
+    // Użycie stałego ziarna dla powtarzalności wyników testowych
+    mt19937 rng(123456); 
 
-    // 3 konfiguracje: liczba wzorców i długość
+    // Konfiguracje eksperymentów: {liczba_wzorców, długość_pojedynczego_wzorca}
     vector<pair<int,int>> configs = {
         {10, 10},
         {50, 12},
         {200, 20}
     };
 
+    // Generowanie i zapisywanie każdego zestawu do oddzielnego pliku
     for(auto &cfg : configs){
         int count = cfg.first;
         int len   = cfg.second;
@@ -90,19 +116,21 @@ int main(int argc, char **argv){
         uniform_int_distribution<int> pos(0, text.size()-len-1);
 
         for(int i=0; i<count; i++){
+            // Pobranie losowego fragmentu z wczytanej sekwencji FASTA
             int p = pos(rng);
             string s = text.substr(p, len);
 
-            // dodajemy dziury
+            // Aplikacja maskowania
             s = add_gaps(s, gap_frac, rng);
 
             pats.push_back(s);
         }
 
+        // Nazewnictwo plików wynikowych: prefix_liczba.txt
         string fname = prefix + "_" + to_string(count) + ".txt";
         save_patterns(pats, fname);
 
-        cerr << "[OK] Saved " << fname << "\n";
+        cerr << "[OK] Saved " << fname << " (Patterns: " << count << ", Len: " << len << ")\n";
     }
 
     return 0;
